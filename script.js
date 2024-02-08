201abf2db950c3d05fff9eb0ff7eea3d03bb8a57
Valdemir rodrigues Goncalves Junior <72047928+ValdemirJunior2020@users.noreply.github.com>
2024-02-08 08:26:05 -0500

    Update script.js

--- script.js
+++ script.js
@@ -8,12 +8,15 @@ function checkPassword() {
 
         // Add your links dynamically here, with styling for spacing and target="_blank"
         document.getElementById('links').innerHTML += `
-            <a href="https://docs.google.com/spreadsheets/d/1a-H17ku4-c1y5rG2P2MAFb4Zvgqws7Z3O1FQ_Sem8XY/edit#gid=1243467041" style="margin-bottom: 10px; display: block;" target="_blank">Old Responses</a><br>
-            <a href="https://docs.google.com/forms/d/e/1FAIpQLSe3nlEgsKlRQIvxLJwsV4QGyXA3qbsGAH_lC68G25-DlBBjlg/viewform" style="margin-bottom: 10px; display: block;" target="_blank">Form 2024</a><br>
-            <a href="https://miro.medium.com/v2/resize:fit:1100/format:webp/0*G0_JqUNK2RfFsN2F.jpg" style="margin-bottom: 10px; display: block;" target="_blank">Do you have a Link?</a><br>
-            <a href="https://docs.google.com/spreadsheets/d/1m6LsOTaCli0s7Tz7_iTF8V2rr6gGHJUkigU6NfpPR2c/edit?resourcekey#gid=975602118;" target="_blank">New Responses 2024</a><br>
+            <a href="https://docs.google.com/spreadsheets/d/1a-H17ku4-c1y5rG2P2MAFb4Zvgqws7Z3O1FQ_Sem8XY/edit#gid=1243467041" style="margin-bottom: 10px; display: block;" target="_blank">Old Responses</a>
+            <a href="https://docs.google.com/forms/d/e/1FAIpQLSe3nlEgsKlRQIvxLJwsV4QGyXA3qbsGAH_lC68G25-DlBBjlg/viewform" style="margin-bottom: 10px; display: block;" target="_blank">Form 2024</a>
+            
+            <a href="https://docs.google.com/spreadsheets/d/1m6LsOTaCli0s7Tz7_iTF8V2rr6gGHJUkigU6NfpPR2c/edit?resourcekey#gid=975602118" style="margin-bottom: 10px; display: block;" target="_blank">New Responses 2024</a>
+            <a href="https://docs.google.com/presentation/d/1ceDoOM9o5h_nSmroHRUc-3bwHi6_uusU6HIOqEqhR1c/edit#slide=id.gc6fa3c898_0_0" style="margin-bottom: 10px; display: block;" target="_blank">HP MBR Jim's Link</a>
+            <a href="https://docs.google.com/forms/d/e/1FAIpQLSe6Zo2XXP-otpbMzScXIwSqv3qFYBmsl2OS7Swo389ZpzCG5w/viewform" style="margin-bottom: 10px; display: block;" target="_blank">2023 Form</a>
+            <a href="https://docs.google.com/spreadsheets/d/1a-H17ku4-c1y5rG2P2MAFb4Zvgqws7Z3O1FQ_Sem8XY/edit#gid=1243467041" style="margin-bottom: 10px; display: block;" target="_blank">2023 Answers</a>
         `;
     } else {
-        alert('Incorrect password. Please try again.');
+        alert('Incorrect password. Please try again, or ask Junior for help .');
     }
 }
