345514b3fdc823a691216e27ef573a6760f79844
Valdemir rodrigues Goncalves Junior <72047928+ValdemirJunior2020@users.noreply.github.com>
2024-02-09 08:20:03 -0500

    Update script.js

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ function checkPassword() {
         document.getElementById('links').innerHTML += `
             <a href="https://docs.google.com/spreadsheets/d/1a-H17ku4-c1y5rG2P2MAFb4Zvgqws7Z3O1FQ_Sem8XY/edit#gid=1243467041" style="margin-bottom: 10px; display: block;" target="_blank">Old Responses</a>
             <a href="https://docs.google.com/forms/d/e/1FAIpQLSe3nlEgsKlRQIvxLJwsV4QGyXA3qbsGAH_lC68G25-DlBBjlg/viewform" style="margin-bottom: 10px; display: block;" target="_blank">Form 2024</a>
-            <a href="https://docs.google.com/spreadsheets/d/1XgZTnQ8nd7V_9tWeRQxE7Yx8B8mzc0GtsciOTm5xhDQ/edit#gid=60603194" style="margin-bottom: 10px; display: block;" target="_blank">ASAP Agents to Q&A</a>
+            <a href="https://docs.google.com/spreadsheets/d/1XgZTnQ8nd7V_9tWeRQxE7Yx8B8mzc0GtsciOTm5xhDQ/edit#gid=60603194" style="margin-bottom: 10px; display: block;" target="_blank">Agents-to-Q-A-ASAP</a>
             <a href="https://docs.google.com/spreadsheets/d/1m6LsOTaCli0s7Tz7_iTF8V2rr6gGHJUkigU6NfpPR2c/edit?resourcekey#gid=975602118" style="margin-bottom: 10px; display: block;" target="_blank">New Responses 2024</a>
             <a href="https://docs.google.com/presentation/d/1ceDoOM9o5h_nSmroHRUc-3bwHi6_uusU6HIOqEqhR1c/edit#slide=id.gc6fa3c898_0_0" style="margin-bottom: 10px; display: block;" target="_blank">HP MBR Jim's Link</a>
             <a href="https://docs.google.com/forms/d/e/1FAIpQLSe6Zo2XXP-otpbMzScXIwSqv3qFYBmsl2OS7Swo389ZpzCG5w/viewform" style="margin-bottom: 10px; display: block;" target="_blank">2023 Form</a>
