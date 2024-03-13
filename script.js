bdaa99ef358bb8c550b0f79bcd4aef7ad93dfdba
Valdemir rodrigues Goncalves Junior <72047928+ValdemirJunior2020@users.noreply.github.com>
2024-03-13 12:37:37 -0400

    Update script.js
    added back the ASAP AGENTS

--- script.js
+++ script.js
@@ -13,11 +13,10 @@ function checkPassword() {
         document.getElementById('links').innerHTML += `
             <div class="column">
                 <!-- Left column links -->                
-                <a href="https://docs.google.com/forms/d/e/1FAIpQLSe3nlEgsKlRQIvxLJwsV4QGyXA3qbsGAH_lC68G25-DlBBjlg/viewform" class="link" target="_blank">Form 2024</a>
-               
-                               
+                <a href="https://docs.google.com/forms/d/e/1FAIpQLSe3nlEgsKlRQIvxLJwsV4QGyXA3qbsGAH_lC68G25-DlBBjlg/viewform" class="link" target="_blank">Form 2024</a>      
                 <a href="https://docs.google.com/presentation/d/1ceDoOM9o5h_nSmroHRUc-3bwHi6_uusU6HIOqEqhR1c/edit#slide=id.gc6fa3c898_0_0" class="link" target="_blank">HP MBR Jim's Link</a>
                 <a href="https://docs.google.com/spreadsheets/d/1pISOhvq7_DxiHVcbkxV1Kf85NOcCj6rntVihhUSkCzI/edit?usp=sharing" class="link" target="_blank">HP WNS Responses certificate</a>
+                <a href="https://docs.google.com/spreadsheets/d/1XgZTnQ8nd7V_9tWeRQxE7Yx8B8mzc0GtsciOTm5xhDQ/edit#gid=60603194" class="link" target="_blank">Agents to Listen in QA</a>
             </div>
 
             <div class="column">
