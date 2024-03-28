5f7101fd739375be79f525050ed35316bb28ea08
Valdemir rodrigues Goncalves Junior <72047928+ValdemirJunior2020@users.noreply.github.com>
2024-03-28 12:51:23 -0400

    Update script.js

--- script.js
+++ script.js
@@ -17,7 +17,8 @@ function checkPassword() {
                 <a href="https://q-a-notes.vercel.app/"  class="link" target="_blank">Drop-Down-reasons</a>                               
                 <a href="https://docs.google.com/presentation/d/1ceDoOM9o5h_nSmroHRUc-3bwHi6_uusU6HIOqEqhR1c/edit#slide=id.gc6fa3c898_0_0" class="link" target="_blank">HP MBR Jim's Link</a>
                 <a href="https://drive.google.com/file/d/1yUR_25xjqSukP9nVj0hvsKX0pYMMkaZi/view?usp=sharing" class="link" target="_blank">Training Guide</a>
-                <a href="https://docs.google.com/spreadsheets/d/1Fe1dVckSGRVvnD8sSZf-6wRqTjOLrK_yL5gkPLP40dQ/edit?usp=sharing" target="_blank">HP WNS Responses certificate</a>
+                <a href="https://docs.google.com/spreadsheets/d/1pISOhvq7_DxiHVcbkxV1Kf85NOcCj6rntVihhUSkCzI/edit?usp=sharing" class="link" target="_blank">HP WNS Responses certificate</a>
+                <a href="https://docs.google.com/spreadsheets/d/17XZLHmCGTOEZKbqW8bk04Pay9Emc3AJ39ElwhOF9_9E/edit?usp=sharing" class="link" target="_blank">Qa-Agents to Listen to</a>
             </div>
 
             <div class="column">
@@ -25,11 +26,10 @@ function checkPassword() {
                 
                 <a href="https://docs.google.com/spreadsheets/d/1a-H17ku4-c1y5rG2P2MAFb4Zvgqws7Z3O1FQ_Sem8XY/edit#gid=1243467041" class="link" target="_blank">2023 Answers</a>
                 <a href="https://q-a-notes.vercel.app/" class="link" target="_blank">Dropdown-discriptions 2024</a>
-                <a href="https://forms.gle/JwKRo6hWLydqLcFN8" class="link" target="_blank">Certification WNS</a> 
-                <a href="https://docs.google.com/spreadsheets/d/1xjwVz9Jmrt3GX2WQe7RPkETzFincwWk2a1vMxYLZZZs/edit?usp=sharing" class="link" target="_blank">New Responses 2024</a>
-                <a href="https://docs.google.com/spreadsheets/d/1h29DVUMt4As3BSzEUyH2O520goHDdF4mbsb9Agcdrfw/edit?usp=sharing" class="link" target="_blank">Weekly - Q&A Calls  (Responses)</a>
-                <a href="https://forms.gle/P9cpF5Kc6DPR3QT16" class="link" target="_blank">Weekly - Q&A Calls</a>
-            </div>
+                <a href="https://forms.gle/JwKRo6hWLydqLcFN8" class="link" target="_blank">Certification WNS</a>
+                
+                
+                 <a href="https://docs.google.com/spreadsheets/d/1xjwVz9Jmrt3GX2WQe7RPkETzFincwWk2a1vMxYLZZZs/edit?usp=sharing" class="link" target="_blank">New Responses 2024</a>
             </div>
         `;
     } else {
