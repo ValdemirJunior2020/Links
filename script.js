296b5e1d97ea22d408128ebb8a67e40ee7d0dca6
Valdemir rodrigues Goncalves Junior <72047928+ValdemirJunior2020@users.noreply.github.com>
2024-03-28 15:21:18 -0400

    Update script.js

--- script.js
+++ script.js
@@ -4,32 +4,39 @@ function checkPassword() {
     // Replace 'qa2024' with the actual password you want
     if (enteredPassword === 'qa2024') {
         document.getElementById('passwordForm').style.display = 'none';
-        document.getElementById('links').style.display = 'block';
-
-        // Clear existing content in the links div
+        document.getElementById('links').style.display = 'flex';
         document.getElementById('links').innerHTML = '';
 
-        // Add the new links
+        // Add the new links and emails
         document.getElementById('links').innerHTML += `
             <div class="column">
-                <!-- Left column links -->                
-                <a href="https://docs.google.com/forms/d/e/1FAIpQLSe3nlEgsKlRQIvxLJwsV4QGyXA3qbsGAH_lC68G25-DlBBjlg/viewform" class="link" target="_blank">Form 2024</a>
-                <a href="https://q-a-notes.vercel.app/"  class="link" target="_blank">Drop-Down-reasons</a>                               
-                <a href="https://docs.google.com/presentation/d/1ceDoOM9o5h_nSmroHRUc-3bwHi6_uusU6HIOqEqhR1c/edit#slide=id.gc6fa3c898_0_0" class="link" target="_blank">HP MBR Jim's Link</a>
-                <a href="https://drive.google.com/file/d/1yUR_25xjqSukP9nVj0hvsKX0pYMMkaZi/view?usp=sharing" class="link" target="_blank">Training Guide</a>
-                <a href="https://docs.google.com/spreadsheets/d/1pISOhvq7_DxiHVcbkxV1Kf85NOcCj6rntVihhUSkCzI/edit?usp=sharing" class="link" target="_blank">HP WNS Responses certificate</a>
-                <a href="https://docs.google.com/spreadsheets/d/17XZLHmCGTOEZKbqW8bk04Pay9Emc3AJ39ElwhOF9_9E/edit?usp=sharing" class="link" target="_blank">Qa-Agents to Listen to</a>
+                <!-- Left column links -->
+                <div class="box">
+                    <a href="https://docs.google.com/forms/d/e/1FAIpQLSe3nlEgsKlRQIvxLJwsV4QGyXA3qbsGAH_lC68G25-DlBBjlg/viewform" class="link" target="_blank">Form 2024</a>
+                    <a href="https://drive.google.com/file/d/1yUR_25xjqSukP9nVj0hvsKX0pYMMkaZi/view?usp=sharing" class="link" target="_blank">Training Guide</a>
+                    <a href="https://docs.google.com/presentation/d/1ceDoOM9o5h_nSmroHRUc-3bwHi6_uusU6HIOqEqhR1c/edit#slide=id.gc6fa3c898_0_0" class="link" target="_blank">HP MBR Jim's Link</a>
+                    <a href="https://q-a-notes.vercel.app/" target="_blank">Drop-Down-reasons</a>
+                </div>
             </div>
-
             <div class="column">
-                <!-- Right column links -->
-                
-                <a href="https://docs.google.com/spreadsheets/d/1a-H17ku4-c1y5rG2P2MAFb4Zvgqws7Z3O1FQ_Sem8XY/edit#gid=1243467041" class="link" target="_blank">2023 Answers</a>
-                <a href="https://q-a-notes.vercel.app/" class="link" target="_blank">Dropdown-discriptions 2024</a>
-                <a href="https://forms.gle/JwKRo6hWLydqLcFN8" class="link" target="_blank">Certification WNS</a>
-                
-                
-                 <a href="https://docs.google.com/spreadsheets/d/1xjwVz9Jmrt3GX2WQe7RPkETzFincwWk2a1vMxYLZZZs/edit?usp=sharing" class="link" target="_blank">New Responses 2024</a>
+                <!-- Middle column links -->
+                <div class="box">
+                    <a href="https://docs.google.com/spreadsheets/d/1a-H17ku4-c1y5rG2P2MAFb4Zvgqws7Z3O1FQ_Sem8XY/edit#gid=1243467041" class="link" target="_blank">2023 Answers</a>
+                    <a href="https://q-a-notes.vercel.app/" class="link" target="_blank">Dropdown-discriptions 2024</a>
+                    <a href="https://forms.gle/JwKRo6hWLydqLcFN8" class="link" target="_blank">Certification WNS</a>
+                    <a href="https://docs.google.com/spreadsheets/d/1Fe1dVckSGRVvnD8sSZf-6wRqTjOLrK_yL5gkPLP40dQ/edit?resourcekey#gid=260355918" class="link" target="_blank">Missing Link</a>
+                </div>
+            </div>
+            <div class="column">
+                <!-- Right column emails -->
+                <div class="box">
+                    <h3>Call Center emails</h3>
+                    <p>WNS - <a href="mailto:HotelPlanner-Operations@wns.com">HotelPlanner-Operations@wns.com</a></p>
+                    <p>Ideal - <a href="mailto:hpstaff@idealccs.com">hpstaff@idealccs.com</a></p>
+                    <p>Buwelo - <a href="mailto:leadership.hotelplanner@buwelo.com">leadership.hotelplanner@buwelo.com</a></p>
+                    <p>Concentrix - <a href="mailto:Delivery_PH_HotelPlanner@concentrix.com">Delivery_PH_HotelPlanner@concentrix.com</a></p>
+                    <p>TEP - <a href="mailto:Teleperformance-HP_Operations@teleperformance.com">Teleperformance-HP_Operations@teleperformance.com</a></p>
+                </div>
             </div>
         `;
     } else {
