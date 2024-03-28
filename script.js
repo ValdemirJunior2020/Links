512f9341544058967d55eb261291e49da71e8e21
Valdemir rodrigues Goncalves Junior <72047928+ValdemirJunior2020@users.noreply.github.com>
2024-03-28 16:23:29 -0400

    Update script.js

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ function checkPassword() {
                     <a href="https://docs.google.com/spreadsheets/d/1a-H17ku4-c1y5rG2P2MAFb4Zvgqws7Z3O1FQ_Sem8XY/edit#gid=1243467041" class="link" target="_blank">2023 Answers</a>
                     <a href="https://q-a-notes.vercel.app/" class="link" target="_blank">Dropdown-discriptions 2024</a>
                     <a href="https://forms.gle/JwKRo6hWLydqLcFN8" class="link" target="_blank">Certification WNS</a>
-                    <a href="https://docs.google.com/spreadsheets/d/1Fe1dVckSGRVvnD8sSZf-6wRqTjOLrK_yL5gkPLP40dQ/edit?resourcekey#gid=260355918" class="link" target="_blank">Missing Link</a>
+                    <a href="https://docs.google.com/spreadsheets/d/1Fe1dVckSGRVvnD8sSZf-6wRqTjOLrK_yL5gkPLP40dQ/edit?resourcekey#gid=260355918" class="link" target="_blank">Wns Re:</a>
                 </div>
             </div>
             <div class="column">
